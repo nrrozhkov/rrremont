4d6ea21544012337ae44e509d68678ed57395c97
nrrozhkov <nrrozhkov@gmail.com>
2021-09-16 17:58:04 +0300

    Внес мелкие стилистические правки, добавил автозакрытие меню при клике на ссылку

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,6 +1,7 @@
 const closeButton = document.querySelector(".main-nav__toggle");
 const mainNav = document.querySelector(".main-nav");
 const openButton = document.querySelector(".page-header__inner-button")
+const links = mainNav.querySelectorAll(".main-nav__item-link")
 
 closeButton.addEventListener("click", function () {
     mainNav.classList.remove("main-nav--opened");
@@ -11,3 +12,10 @@ openButton.addEventListener('click', function () {
   mainNav.classList.remove("main-nav--closed");
   mainNav.classList.add("main-nav--opened");
 })
+
+for (let item of links) {
+  item.addEventListener('click', function() {
+    mainNav.classList.remove("main-nav--opened");
+    mainNav.classList.add("main-nav--closed")
+  })
+}
